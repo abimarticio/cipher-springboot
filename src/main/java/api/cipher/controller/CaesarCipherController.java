7217e3422a3e72bd9fba83b38140b8d8f02c6f06
Abi <marticiobgl@gmail.com>
2020-10-20 15:56:22 +0800

    chore: add GET mapping for encryption function

--- src/main/java/api/cipher/controller/CaesarCipherController.java
+++ src/main/java/api/cipher/controller/CaesarCipherController.java
@@ -1,9 +1,17 @@
 package api.cipher.controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.cipher.model.CaesarCipher;
 
 @RestController
 public class CaesarCipherController {
 
+	@GetMapping("/encrypt")
+	public String encryptText(@RequestParam(value="text", required=true) String text) {
+		CaesarCipher caesarCipher = new CaesarCipher(4);
+		String encryptedText = caesarCipher.encryptText(text);
+		return encryptedText;
+	}
 }
