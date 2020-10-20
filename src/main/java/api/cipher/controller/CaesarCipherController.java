1cc366ba278efea8ff3cf2c8cbe3bcbaff577269
Abi <marticiobgl@gmail.com>
2020-10-20 22:57:10 +0800

    chore: use Map as GET params for encryption

--- src/main/java/api/cipher/controller/CaesarCipherController.java
+++ src/main/java/api/cipher/controller/CaesarCipherController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import api.cipher.model.CaesarCipher;
 
+import java.util.Map;
+
 @RestController
 public class CaesarCipherController {
 
 	@GetMapping("/encrypt")
-	public String encryptText(@RequestParam(value="text", required=true) String text) {
-		CaesarCipher caesarCipher = new CaesarCipher(4);
+	public String encryptText(@RequestParam Map <String, String> requestParameters) {
+		int key = Integer.valueOf(requestParameters.get("key"));
+		String text = requestParameters.get("text");
+		CaesarCipher caesarCipher = new CaesarCipher(key);
 		String encryptedText = caesarCipher.encryptText(text);
 		return encryptedText;
 	}
