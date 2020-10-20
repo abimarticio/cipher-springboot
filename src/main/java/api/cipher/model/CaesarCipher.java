d20c09f2ac5090659c9e85d40961e2eedadf6099
Abi <marticiobgl@gmail.com>
2020-10-20 15:03:12 +0800

    docs: add Javadoc for constructor

--- src/main/java/api/cipher/model/CaesarCipher.java
+++ src/main/java/api/cipher/model/CaesarCipher.java
@@ -5,6 +5,10 @@ public class CaesarCipher {
 	private int key;
 	private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
+	/**
+	 * Constructs a Caesar cipher object.
+	 * @param key The key to use for encryption or decryption.
+	 */
 	public CaesarCipher(int key) {
 		this.key = key;
 	}
