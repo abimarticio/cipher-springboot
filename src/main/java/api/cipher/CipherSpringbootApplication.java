ad5f42310066f8fdcc5e0d0ef0389c335da8d468
Abi <marticiobgl@gmail.com>
2020-10-20 14:47:39 +0800

    chore: add template spring boot app

---
+++ src/main/java/api/cipher/CipherSpringbootApplication.java
@@ -0,0 +1,13 @@
+package api.cipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/cipher/CipherSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package api.cipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
