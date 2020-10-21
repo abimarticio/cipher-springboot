7d30f6a52bba357fb3001dacae04f0bef12ea0c4
Abi <marticiobgl@gmail.com>
2020-10-21 23:52:01 +0800

    chore: define function for encryption and decryption

--- src/main/java/api/cipher/model/AtbashCipher.java
+++ src/main/java/api/cipher/model/AtbashCipher.java
@@ -4,4 +4,13 @@ public class AtbashCipher {
 	
 	private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
+	public String encryptDecryptText(String text) {
+		char[] encryptedDecryptedText = text.toUpperCase().toCharArray();
+		for(int index = 0; index < encryptedDecryptedText.length; index++) {
+			int letterIndex  = ALPHABET.indexOf(encryptedDecryptedText[index]);
+			int cipherIndex = ((ALPHABET.length() - 1) * letterIndex + (ALPHABET.length() - 1)) % ALPHABET.length();
+			encryptedDecryptedText[index] = ALPHABET.charAt(cipherIndex);
+		}
+		return String.valueOf(encryptedDecryptedText);
+	}
 }
