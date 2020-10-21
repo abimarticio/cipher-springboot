728dc0adfc24e281a5453219976793b7fa6a60dd
Abi <marticiobgl@gmail.com>
2020-10-22 00:47:31 +0800

    chore: add GET mapping for encryption

--- src/main/java/api/cipher/controller/AtbashCipherController.java
+++ src/main/java/api/cipher/controller/AtbashCipherController.java
@@ -1,9 +1,17 @@
 package api.cipher.controller;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.cipher.model.AtbashCipher;
 
 @RestController
 public class AtbashCipherController {
-
+	@GetMapping("/atbash/encrypt")
+	public String encryptText(@RequestParam(value="text", required=true) String text) {
+		AtbashCipher atbashCipher = new AtbashCipher();
+		String encryptedText = atbashCipher.encryptDecryptText(text);
+		return encryptedText;
+	}
 }
