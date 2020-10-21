d96f58a2f8849a480e706f7d9f19dce456043dfe
Abi <marticiobgl@gmail.com>
2020-10-22 00:51:34 +0800

    chore: add GET mapping for decryption

--- src/main/java/api/cipher/controller/AtbashCipherController.java
+++ src/main/java/api/cipher/controller/AtbashCipherController.java
@@ -8,10 +8,18 @@ import api.cipher.model.AtbashCipher;
 
 @RestController
 public class AtbashCipherController {
+	
 	@GetMapping("/atbash/encrypt")
 	public String encryptText(@RequestParam(value="text", required=true) String text) {
 		AtbashCipher atbashCipher = new AtbashCipher();
 		String encryptedText = atbashCipher.encryptDecryptText(text);
 		return encryptedText;
+	}
+	
+	@GetMapping("/atbash/decrypt")
+	public String decryptText(@RequestParam(value="text", required=true) String text) {
+		AtbashCipher atbashCipher = new AtbashCipher();
+		String decryptedText = atbashCipher.encryptDecryptText(text);
+		return decryptedText;
 	}
 }
